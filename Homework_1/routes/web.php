bffa49bf3580691c44eb3b342693897e4ed61eed
lim1999 <rean.it123@gmail.com>
2019-11-23 22:25:44 +0700

    d

--- Homework_1/routes/web.php
+++ Homework_1/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['namespace' => 'Front'],function(){
+    Route::resource('property-type','PropertyTypeController');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
